84c52cddffc938c66a5af482e4be81d068962b83
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-02-11 16:12:35 +0530

    Added code for ceiling and floor of a number

---
+++ Arrays/Searching/Binary-Search/Ceiling.java
@@ -0,0 +1,32 @@
+//Find the smallest number greater than the target
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,6,8,14,16,18};
+        int target = 15;
+
+        int result = CeilingNum(arr, target);
+
+        System.out.print("The ceiling of the number" + target + "is  : "+ arr[result]);
+    }
+
+    static int CeilingNum(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end) {
+            int mid = start + (end-start)/2;
+
+            if(target == arr[mid]){
+                return mid;
+            } else if(target > arr[mid]){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+
+            
+        }
+        return start;
+    }
+}
