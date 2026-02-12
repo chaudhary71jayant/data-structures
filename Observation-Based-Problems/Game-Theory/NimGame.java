9892af13633b7fda1d1c32885bbb2041f36b806d
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-02-12 22:00:07 +0530

    Leetcode Problem no. 292

---
+++ Observation-Based-Problems/Game-Theory/NimGame.java
@@ -0,0 +1,19 @@
+// leetcode problem no
+
+public class NimGame {
+   public static void main(String[] args) {
+        int n = 4;
+
+    if(canWinNim(n)){
+        System.out.print("You can win the game.");
+    } else {
+        System.out.print("You can't win the game.");
+    }
+   }
+
+    static boolean canWinNim(int n) {
+        if(n%4==0) return false;
+        return true;
+    }
+    
+}
