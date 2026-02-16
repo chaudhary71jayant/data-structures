d75e6187ed5e6296a260b65e9cc90abf15cb42ce
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-02-16 21:37:51 +0530

    Problem 162

---
+++ Arrays/Searching/Binary-Search/FindPeakElement.java
@@ -0,0 +1,24 @@
+//Problem 162 leetcode Find Peak Element
+
+public class FindPeakElement {
+    static int findPeakElement(int[] nums) {
+        int start =0;
+        int end = nums.length-1;
+
+        while(start < end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] > nums[mid+1]){
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,5,6,4};
+
+        System.out.print("The peak element in the array is : " + findPeakElement(nums));
+    }
+}
