bd54dfa3ce0e5272d1e633638e3fe324d6e3a844
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-03-02 22:56:05 +0530

    Leetcode Problem 88

---
+++ Arrays/Two-PointersXslidingwindow/MergeTwoSortedArrays.java
@@ -0,0 +1,32 @@
+//leetcode problem number 88 merge two sorted arrays
+
+public class MergeTwoSortedArrays{
+    static void merge(int[] nums1,int m,int[] nums2,int n){
+        int i=m-1,j=n-1,k=m+n-1;
+
+        while(j>=0){
+            if(i>=0 && nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.print("The new merged array is : ");
+        for(int num : nums1){
+            System.out.print(num + " ");
+        }
+
+    }
+}
