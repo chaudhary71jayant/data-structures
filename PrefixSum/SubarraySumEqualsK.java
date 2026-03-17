656fe388bbb20450e57b024983d178eb9358c0ff
Chaudharyjayant <chaudharyjayant980@gmail.com>
2026-03-17 13:45:41 +0530

    Prefix sum leetcode 560

---
+++ PrefixSum/SubarraySumEqualsK.java
@@ -0,0 +1,41 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Leetcode 560
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums,int k){
+        Map<Integer,Integer> map = new HashMap<>();
+        int count=0;
+        int[] prefixSum = new int[nums.length];
+
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];        
+        }
+
+        for(int j=0;j<nums.length;j++){
+            if(prefixSum[j] == k){
+                count++;
+            }
+
+            int val = prefixSum[j] - k;
+            if(map.containsKey(val)){
+                count += map.get(val);
+            }
+
+            if(!map.containsKey(prefixSum[j])){
+                map.put(prefixSum[j], 0);
+            }
+            map.put(prefixSum[j], map.getOrDefault(prefixSum[j], 0)+1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        int k = 3;
+
+        System.out.print("The number of subarray with sum equals k are : "+ subarraySum(nums, k));
+    }
+}
